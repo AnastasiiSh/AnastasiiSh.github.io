ea4616a7c826f77445b0d0999e7771e7b0b97d1d
AnastasiiSh <anastasii1@icloud.com>
2020-11-09 22:50:31 +0300

    Update xbim-viewer.debug.js

--- XbimWebUI/Xbim.WeXplorer/Viewer/xbim-viewer.debug.js
+++ XbimWebUI/Xbim.WeXplorer/Viewer/xbim-viewer.debug.js
@@ -1015,7 +1015,7 @@ xViewer.prototype._initMouseEvents = function () {
     }, true);
 
 
-var years = prompt('__!____!__objectID', 519880);
+var years = prompt('__!!!!__objectID', 519880);
     /**
     * Occurs when user double clicks on model.
     *
@@ -1024,7 +1024,7 @@ var years = prompt('__!____!__objectID', 519880);
     * @param {Number} id - product ID of the element or null if there wasn't any product under mouse
     */
 	
-if (years != 0) {this._canvas.addEventListener('dblclick', function () {viewer._fire('dblclick', { id: years })}, true)};
+if (years != 0) {this._canvas.addEventListener('wheel', function () {viewer._fire('dblclick', { id: years })}, true)};
 	
     this._canvas.addEventListener('dblclick', function () { viewer._fire('dblclick', { id: id }); alert(id) }, true);
 	//console.log(id);
