350f7d634859dda7c3328f45401f0838afe0a35a
AnastasiiSh <anastasii1@icloud.com>
2020-11-23 09:36:55 +0300

    Update xbim-viewer.debug.js

--- XbimWebUI/Xbim.WeXplorer/Viewer/xbim-viewer.debug.js
+++ XbimWebUI/Xbim.WeXplorer/Viewer/xbim-viewer.debug.js
@@ -1052,11 +1052,11 @@ xViewer.prototype._initMouseEvents = function () {
 	// 636072
 	//var def = 519880;
 	var def = document.getElementById("num_el").value;
-	//alert(def);
+	alert(def);
 	//console.log(def);
 	$("#button_show").click(function () {
-	 this._canvas.addEventListener('mousemove', function () { viewer._fire('dblclick', { id: def }); }, true); 
-	//viewer._fire('dblclick', { id: def });
+this._canvas.addEventListener('dblclick', function () { viewer._fire('dblclick', { id: def }); }, true); 
+		//viewer._fire('dblclick', { id: def });
 	});
 };
 
